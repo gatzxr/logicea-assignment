e64a5e476404f52583b87fe0c940280436e96228
gatzxr <regkatzi@gmail.com>
2023-10-05 02:22:24 +0300

    fix dependecy issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,7 @@
-const Layout = () => <div>Test</div>;
+const Layout = () => (
+  <div className="flex h-[50px] items-center justify-between bg-blue-200 ">
+    Test
+  </div>
+);
 
 export default Layout;
