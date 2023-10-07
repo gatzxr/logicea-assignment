db1a1aa15a4ea76c7ce458f4b2f8bf5e293a5e30
gatzxr <regkatzi@gmail.com>
2023-10-07 13:48:06 +0300

    Refactor index.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import Layout from 'components/Layout';
 import { EditJoke, Jokes, Login, NotFound } from 'components/Pages';
 
+import { AuthContextProvider } from 'contexts/AuthContext';
+import { AxiosContextProvider } from 'contexts/AxiosContext';
+import { ThemeContextProvider } from 'contexts/ThemeContext';
+
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/jokes" />} />
-      <Route path="/jokes" element={<Layout />}>
-        <Route index element={<Jokes />} />
-        <Route path="new" element={<EditJoke isNew />} />
-        <Route path=":id" element={<EditJoke isNew={false} />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <AxiosContextProvider baseURL="https://retoolapi.dev/vcv4zy/">
+          <ThemeContextProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Navigate to="/jokes" />} />
+                <Route path="/jokes" element={<Layout />}>
+                  <Route index element={<Jokes />} />
+                  <Route path="new" element={<EditJoke isNew />} />
+                  <Route path=":id" element={<EditJoke isNew={false} />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </ThemeContextProvider>
+          <ToastContainer />
+        </AxiosContextProvider>
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,10 @@
 import App from 'App';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-
-import { AuthContextProvider } from 'contexts/AuthContext';
-import { AxiosContextProvider } from 'contexts/AxiosContext';
-import { ThemeContextProvider } from 'contexts/ThemeContext';
 
 import './index.css';
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <AuthContextProvider>
-      <AxiosContextProvider baseURL="https://retoolapi.dev/vcv4zy/">
-        <ThemeContextProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ThemeContextProvider>
-        <ToastContainer />
-      </AxiosContextProvider>
-    </AuthContextProvider>
-  </QueryClientProvider>
-);
+root.render(<App />);
